2469f5e2df60de7f0274a12f5c0df057d1d344f6
Chirag Guliani <chirag.guliani@gmail.com>
2016-04-13 19:19:34 -0500

    New Code

--- src/automation/framework/SelectActiveBid.java
+++ src/automation/framework/SelectActiveBid.java
@@ -29,43 +29,39 @@ public class SelectActiveBid {
 		
 		//login button click below:
 		driver.findElement(By.xpath("html/body/div[1]/table[3]/tbody/tr/td[2]/div/div[3]/button[1]")).click();
+				
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		
-		driver.get("http://suppliers.qa.inwk.com/#home");
 		
-		//driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		
-		WebElement menuBar = driver.findElement(By.xpath("/html/body/table/tbody/tr/td/div/table[1]/tbody/tr[1]"));
+		/*WebElement menuBar = driver.findElement(By.xpath("/html/body/table/tbody/tr/td/div/table[1]/tbody/tr[1]"));
 		List<WebElement> childs = menuBar.findElements(By.xpath(".//*"));
 		
 		for (WebElement element : childs) {
 		    System.out.println(element);
 		} 
+		*/
 		
-		WebElement elebids = driver.findElement(By.xpath(".//td[contains(text(), 'Bids')]"));
+		WebElement elebids = driver.findElement(By.xpath("//tr/td[contains(text(), 'Bids')]"));
+		
 		Actions action = new Actions(driver);
 		action.moveToElement(elebids).build().perform();
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		
-		
-		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+				
+		//WebDriverWait wait = new WebDriverWait(driver, 5); 
+		//wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='subNav_music_menu']/tbody/tr[2]/td[1]/a[1]")));  // until this submenu is found
+
+		//identify menu option from the resulting menu display and click
+		WebElement menuOption = driver.findElement(By.xpath("/html/body/div[3]/div/table/tbody/tr[2]/td[2]/div/div/table/tbody/tr[2]/td"));
+		menuOption.click(); 
 		
 		//if ( !driver.findElement(By.id("gwt-uid-106")).isSelected() )
 		//{
 		//driver.findElement(By.xpath("/html/body/table/tbody/tr/td/div/table[2]/tbody/tr/td/table/tbody/tr/td/div/table[2]/tbody/tr[3]/td/table/tbody/tr[4]/td/span/label")).click();;
 		
-		//WebElement activeBidMenu = driver.findElement(By.xpath("//*[@id="gwt-uid-168]);
-		
-		WebElement activeBidMenu = driver.findElement(By.xpath("/html/body/table/tbody/tr/td/div/table[1]/tbody/tr[1]/td/div//*[text()[contains(., 'Active Bids')]"));
-		activeBidMenu.click();
+	//	WebElement activeBidMenu = driver.findElement(By.xpath("/html/body/table/tbody/tr/td/div/table[1]/tbody/tr[1]/td/div//*[text()[contains(., 'Active Bids')]"));
+	//	activeBidMenu.click();
 
 		
-		//}
-		
-		//select a bid to bid on:
-		//driver.findElement(By.xpath("//*[contains(text(), 'Bid On This Item')]")).click();
-		
-		//driver.getPageSource().contains("text to search");
-
 	}
 
 }
